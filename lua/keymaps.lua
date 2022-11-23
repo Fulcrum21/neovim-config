f30305927ad0dba6fef80917fb28acfa8bd57f84
Jambi <fulcrumjambi1917@gmail.com>
2022-11-24 00:25:11 +0400

    Modified the creation of keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,43 +1,51 @@
-local keymap = vim.keymap.set
+local setmap = vim.keymap.set
 local opts = {noremap = true, silent = true}
+
+local maps = { i = {}, n = {}, v = {}, }
 
 
 -- Leader key
 -- General
-keymap('i', 'jk', '<ESC>', opts)
-keymap('i', 'jj', '<ESC>', opts)
-keymap('n', '<leader>q', ':q!<CR>', opts)
-keymap('n', '<leader>h', ':nohl<CR>', opts)
-keymap('n', '<leader>w', ':w<CR>', opts)
-keymap('n', '<leader>c', ':close<CR>', opts)
-keymap('n', '<leader>ft', ':!alacritty --working-directory . 2> /dev/null & disown<CR><CR>', opts) -- open terminal in another window
-keymap('v', '<Space><Space>', '<Esc>', opts) -- exit visual mode with two spaces
+maps.i['jk'] = { '<ESC>', desc = "Exit Insert Mode"}
+maps.i['jj'] = { '<ESC>', desc = "Exit Insert Mode"}
+maps.n['<leader>q'] = { ':q!<CR>', desc = "Force Quit"}
+maps.n['<leader>w'] = { ':w!<CR>', desc = "Save File"}
+maps.n['<leader>h'] = { ':nohl<CR>', desc = "Removes search highlighting"}
+maps.n['<leader>c'] = { ':close<CR>', desc = "Closes buffer"}
+maps.n['<leader>ft'] = { ':!prime-run alacritty msg create-window --working-directory . & disown<CR><CR>', desc = "Opens new terminal"}
+maps.v['<Space><Space>'] = { '<Esc>', desc = "Exits visual mode"}
 
 -- Indentation
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
+maps.v['<'] = {'<gv', desc = "indent selected line"}
+maps.v['>'] = {'>gv', desc = "indent selected line"}
 
 -- Buffer movement
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
+maps.n['<C-j>'] = { '<C-w>j', desc = "easier split navigation"}
+maps.n['<C-h>'] = { '<C-w>h', desc = "easier split navigation"}
+maps.n['<C-k>'] = { '<C-w>k', desc = "easier split navigation"}
+maps.n['<C-l>'] = { '<C-w>l', desc = "easier split navigation"}
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+maps.v["<A-j>"] = { ":m '>+1<CR>gv-gv", desc = "Move Selected Text Down" }
+maps.v["J"] = { ":m '>+1<CR>gv-gv", desc = "Move Selected Text Down" }
+maps.v["<A-k>"] = { ":m '<-2<CR>gv-gv", desc = "Move Selected Text Down" }
+maps.v["K"] = { ":m '<-2<CR>gv-gv", desc = "Move Selected Text Down" }
+maps.v["p"] = { '"_dP', desc = "Pastes text without saving to clipboard"}
 
 -- Telescope
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>fw", ":Telescope live_grep<CR>", opts)
+maps.n["<leader>ff"] = { ":Telescope find_files<CR>", desc = "Find Files" }
+maps.n["<leader>fw"] = {":Telescope live_grep<CR>", desc = "Find Word" }
 
 -- Commenting
-keymap("n", "<leader>/", ":lua require('Comment.api').toggle.linewise.current()<CR>", opts)
-keymap("v", "<leader>/", "<Esc>:lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", opts)
+maps.n["<leader>/"] = {":lua require('Comment.api').toggle.linewise.current()<CR>", desc = "Comment line" }
+maps.v["<leader>/"] = {"<Esc>:lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", desc = "Comment selected lines" }
+
+
+for mode, mappings in pairs(maps) do
+    for keymap, options in pairs(mappings) do
+        local cmd = options[1]
+        local keymap_opts = opts
+        keymap_opts = vim.tbl_deep_extend("force", options, keymap_opts)
+        keymap_opts[1] = nil
+        setmap(mode, keymap, cmd, keymap_opts)
+    end
+end
